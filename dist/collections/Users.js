64e6cb658b54e884ddf11848e348966a9c78a1c6
wirapramuja <wiraapramuja44@gmail.com>
2023-12-09 18:59:53 +0700

    first commit

---
+++ dist/collections/Users.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Users = void 0;
+exports.Users = {
+    slug: 'users',
+    auth: {
+        verify: {
+            generateEmailHTML: function (_a) {
+                var token = _a.token;
+                return "<a href=\"".concat(process.env.NEXT_PUBLIC_SERVER_URL, "/verify-email?token=").concat(token, "\">\n          Verify account\n        </a>");
+            }
+        }
+    },
+    access: {
+        read: function () { return true; },
+        create: function () { return true; }
+    },
+    fields: [
+        {
+            name: 'role',
+            required: true,
+            admin: {
+                condition: function () { return false; }
+            },
+            type: 'select',
+            options: [
+                { label: 'Admin', value: 'admin' },
+                { label: 'User', value: 'user' }
+            ]
+        }
+    ]
+};
